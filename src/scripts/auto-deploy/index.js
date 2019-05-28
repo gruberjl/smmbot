422e2ab9af4d1d7111329d166b89f4b533c5b2c6
John Gruber <john.gruber@gitbit.org>
2019-05-27 20:54:54 -0400

    auto

--- src/scripts/auto-deploy/index.js
+++ src/scripts/auto-deploy/index.js
@@ -2,8 +2,13 @@ import simpleGit from 'simple-git'
 
 export const autoDeploy = async () => {
   const git = simpleGit(__dirname)
+
   git.diffSummary((err, r) => {
-    console.log(err)
-    console.log(r)
+    if (r.files.length > 0) {
+      console.log('pull new')
+      console.log(r)
+    } else {
+      console.log('do nothing')
+    }
   })
 }
