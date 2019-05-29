99916287af0ac9cf50e48055105cf7bba8031cac
John Gruber <john.gruber@gitbit.org>
2019-05-29 12:41:10 -0400

    added isReadyToShare

--- src/lib/db/accounts.js
+++ src/lib/db/accounts.js
@@ -1,11 +1,18 @@
+import moment from 'moment'
 import {getCollection, set, get, remove, allDocs} from './lib'
 
 const dbName = 'accounts'
+
+const isReadyToShare = (account) => {
+  const nextShare = moment(account.lastShare).add(account.nextShare, 'minutes')
+  return nextShare.isSameOrBefore(moment())
+}
 
 export const accounts = {
   getCollection,
   set: set(dbName),
   get: get(dbName),
   remove: remove(dbName),
-  allDocs: allDocs(dbName)
+  allDocs: allDocs(dbName),
+  isReadyToShare
 }
